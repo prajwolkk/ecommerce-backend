6ed549c8634a81716e9033fe2813b607c3bf33f2
kharelprajwol <prajwolkharel04@gmail.com>
2026-03-14 02:53:42 +0545

    feat(prisma): add PrismaModule + PrismaService, export for injection, integrate into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './health/health.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [HealthModule, PrismaModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
